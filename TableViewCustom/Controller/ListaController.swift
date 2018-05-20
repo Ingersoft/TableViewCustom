db302e9f2dff85c749a3c80a28210234947050f5
Unknown <ingermanm@hotmail.com>
2018-05-19 20:37:31 -0600

    Tarea clase 7

---
+++ TableViewCustom/Controller/ListaController.swift
@@ -0,0 +1,46 @@
+//
+//  ListaController.swift
+//  TableViewCustom
+//
+//  Created by Inger Montenegro on 5/19/18.
+//  Copyright © 2018 Inger Montenegro. All rights reserved.
+//
+
+import Foundation
+import UIKit
+    
+class ListaController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+        
+        var teams : [String] = []
+        
+        
+        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return teams.count
+        }
+        
+        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            let cell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.subtitle, reuseIdentifier: "mycell")
+            cell.textLabel?.text = teams[indexPath.row]
+            cell.imageView!.image = UIImage(named: teams[indexPath.row])
+            
+            if teams[indexPath.row] == "Barcelona" {
+//                cell.textLabel?.text = "Mamones y Llorones"
+                cell.detailTextLabel?.text = "Mamones y Llorones"
+            }
+            return cell
+        }
+        
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            // Do any additional setup after loading the view, typically from a nib.
+            teams = ["Atletico de Madrid", "Barcelona", "Deportivo de la Coruña", "Las Palmas", "Malaga", "Rayo Vallecano", "Sporting", "Real Sociedad", "Espanyol", "Mallorca", "Valladolid", "Eibar",  "Ponferradina", "Albacete"]
+        }
+        
+        override func didReceiveMemoryWarning() {
+            super.didReceiveMemoryWarning()
+            // Dispose of any resources that can be recreated.
+        }
+        
+        
+}
+
